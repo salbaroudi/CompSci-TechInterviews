4d0a3acf79a649d06275f20c39e2dd78f08c1fb6
User <sean.al.baroudi@gmail.com>
2022-04-18 22:26:41 +0100

    Placing Flowers Problem

--- LeetCode/Greedy/p605_placeflowers.js
+++ LeetCode/Greedy/p605_placeflowers.js
@@ -1,38 +1,92 @@
 var canPlaceFlowers = function(flowerbed, n) {
   let doable = false;
+  let zeroRun = 0;
 
-  //Base Cases:
-  if ( (n == 0) || ((flowerbed.length == 1) && (flowerbed[0] == 0)) || ((flowerbed.length == 2) && (flowerbed[0] == 0) && (flowerbed[1] == 0) && (n == 1)) )  { doable=true; }
-  else {
-      for (let i = 2; i < flowerbed.length; i++) {
-          if ((flowerbed[i-2] == 0) && (flowerbed[i-1] == 0) && (flowerbed[i] == 0)) { n-=1; }
-          if (n == 0) { doable = true; break;}
-      }
+  //Small True Cases
+  if (n == 0) {doable = true;}
+  else if ((flowerbed.length < 3) && (flowerbed.reduce((a, b) => a + b, 0) == 0) && (n == 1)) {
+    doable = true;
   }
-
+  else if (flowerbed.length > 3) { //General Case - won't work with L = 1 or 2.
+        for (let bed of flowerbed) {
+            if (bed == 0) {
+              zeroRun +=1;
+            }
+            else {
+                n-=Math.max(0,Math.ceil((zeroRun - 2)/2));
+                zeroRun = 0;
+            }
+        }
+        //Case: Rollover case of a run of zeros at the end
+        n-=Math.max(0, (Math.ceil((zeroRun - 2)/2)));
+        if ((zeroRun > 0) && (zeroRun % 2 == 0)) { n-=1;} //our odd formula needs patched up.
+        if (n <= 0) {
+            doable = true;
+        }
+  }
   return doable;
-
 };
+//Gives true instead of false!
+console.log(canPlaceFlowers([1,0,0,0,0,0,0,0,0,0,0,1,0,1,0,1,0,1,0,1,0,0,0],6));
+
+/* Test Cases:
+[0]
+1
+[1]
+0
+[0,0]
+0
+[0,0]
+1
+[0,1]
+1
+[1,0]
+1
+[1,0]
+0
+[1,0,0,0,1]
+1
+[1,0,0,0,1]
+2
+[1,0,0,0,1,0,0,0,1,0,0,0,1]
+3
+[1,0,0,0,1,0,0,0,1,0,0,0,1]
+4
+[1,0,0,0,0,0,0,0,0,0,0,1,0,1,0,1,0,1,0,1,0,0,0]
+5
+[1,0,0,0,0,0,0,0,0,0,0,1,0,1,0,1,0,1,0,1,0,0,0]
+6
+[1,0,0,0,0,0,0,0,0,0,0,1,0,1,0,1,0,1,0,1,0,0,0,0]
+6
+[1,0,0,0,0,0,0,0,0,0,0,1,0,1,0,1,0,1,0,1,0,0,0,0,1]
+5
+[0,0,1,0,1]
+1
+[0,1,0,1]
+1
+[0,0,0,1,0,1]
+1
+[0,0,0,0,1,0,1]
+1
+[0,0,0,0,1,0,1]
+2
+[0,0,0,0,1,0,1]
+3
+[1,0,0]
+1
+[0,1,0]
+1
+[0,0,1]
+1
+
+*/
+
+
+
 
 /* Corner Cases:
 - zero length array - handled
 - non 1/0 number - handled
 - two adjacent flowers - handled
 - n > flowerbed length - handled
-
-Test Cases:
-[0]
-0
-[0]
-1
-[0,0]
-1
-[0,0,0]
-1
-[1,0,0,0,1]
-1
-[1,0,0,0,1]
-2
-
-
 */
